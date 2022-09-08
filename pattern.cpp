a7125937061d7f1cc0a89fdff7353a22c92429a1
Venkatasai51 <112601035+Venkatasai51@users.noreply.github.com>
2022-09-08 08:15:40 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main()
+{
+   int a=153
+   {
+   	sum=0;
+   	while(a<0)
+   {
+   	      n=a%10;
+   	      sum=sum+n;
+   	      a=n/10;
+      }
+   }
+   std::cout<<a;
+   return 0;
+}
